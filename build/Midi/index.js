ea8121bf0f6b2110340e47ebc5e2504ab1b9f733
cmoileo <106542483+cmoileo@users.noreply.github.com>
2024-02-04 14:09:37 +0100

    Delete build directory

--- build/Midi/index.js
+++
@@ -1,4 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const Midi_1 = require("./Midi");
-exports.default = Midi_1.Midi;
--- build/Tempo/index.js
+++
@@ -1,4 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const Tempo_1 = require("./Tempo");
-exports.default = Tempo_1.Tempo;
